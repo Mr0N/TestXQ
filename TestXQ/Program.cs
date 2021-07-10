5a05c978c85b567a386e5a8f5a86fed89de136a0
new_user <new_user@gmail.com>
2021-07-10 22:54:22 +0300

    -

---
+++ TestXQ/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TestXQ
+{
+    class BaseX
+    {
+        public void Start()
+        {
+            if (this is GoodY)
+            {
+                var result = this as IGoodInterface;
+                result.StartGoodWorking();
+            }
+        }
+    }
+    class GoodY : BaseX, IGoodInterface
+    {
+        public void StartGoodWorking() {
+            Console.WriteLine("GoodY StartGoodWorking");
+        }
+    }
+    interface IWork
+    {
+        public void StartWorkingWork();
+    }
+    interface IGoodInterface
+    {
+        public void StartGoodWorking();
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new GoodY().Start();
+            Console.ReadKey();
+        }
+    }
+}
